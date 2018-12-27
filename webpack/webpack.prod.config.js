5612038bc05551bdf105605b6fac336bb7c26e72
Alejandro López <alejandro.l@globant.com>
2018-12-27 14:17:52 -0600

    prod

---
+++ webpack/webpack.prod.config.js
@@ -0,0 +1,46 @@
+var webpack = require('webpack');
+var path = require('path');
+var autoprefixer = require('autoprefixer');
+var precss = require('precss');
+
+var parentDir = path.join(__dirname, '../');
+
+module.exports = {
+    entry: [
+        path.join(parentDir, 'index.js')
+    ],
+    mode: 'production',
+    plugins: [
+      new webpack.LoaderOptionsPlugin({
+        options: {
+          context: __dirname,
+          postcss: [
+            autoprefixer
+          ]
+        }
+      })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/, exclude: /node_modules/, loader: 'babel-loader'
+            },
+            {
+                test: /\.scss$/,
+                loaders: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
+            },
+            {
+                test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                use: 'url-loader?limit=10000'
+            },
+            {
+                test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+                use: 'file-loader'
+            }
+        ]
+    },
+    output: {
+        path: parentDir + '/dist',
+        filename: 'bundle.js'
+    }
+}
